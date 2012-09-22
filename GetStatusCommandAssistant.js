c8c9897a10d695e04edd9b71bb25bded66a052f2
Steve Lemke <steve.lemke@palm.com>
2012-09-21 18:03:59 -0700

    Release pmnetconfigmanager-stub 2.0.0 to the public
    Open-webOS-DCO-1.0-Signed-off-by: Steve Lemke <steve.lemke@palm.com>
    
    Change-Id: I4e4af301e9682aa75d7df23350215afa1eeb3129

---
+++ GetStatusCommandAssistant.js
@@ -0,0 +1,48 @@
+// @@@LICENSE
+//
+//      Copyright (c) 2012 Hewlett-Packard Development Company, L.P.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// LICENSE@@@
+
+var GetStatusCommandAssistant = function(){
+}
+
+GetStatusCommandAssistant.prototype.run = function(future) {
+	//console.log(">>>>>>> in GetStatusCommandAssistant");
+	//console.log ("%j", this.controller.args);
+
+	future.result = {
+		"returnValue":true,
+		"isInternetConnectionAvailable":true,
+		"wifi":{
+			"state":"connected",
+			"ipAddress":"192.168.0.0",
+			"interfaceName":"eth0",
+			"ssid":"Open webOS",
+			"bssid":"Open webOS",
+			"networkConfidenceLevel":"excellent",
+			"onInternet":"yes"
+		},
+		"wan":{
+			"state":"disconnected"
+		},
+		"vpn":{
+			"state":"disconnected"
+		},
+		"bridge":{
+			"state":"disconnected"
+		}
+	};
+}
